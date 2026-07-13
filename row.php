061745c4ba28402cb6f9bddbb7f72b92156b34b4
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-07-13 11:52:48 +0800

    push

--- row.php
+++ row.php
@@ -13,7 +13,7 @@ if (isset($_POST['hantar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jumlah Kotak</title>
+    <title>Jumlah row dan kolumn</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
